f28e4832bcbaccc0d91cb41e1a0d95d9526ce079
yosafat-hashmicro <64425747+yosafat-hashmicro@users.noreply.github.com>
2020-08-01 12:24:58 +0700

    first uplaod

---
+++ src/views/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home">
+    <Header />
+    <CarouselHome/>
+    <ItemHome/>
+    <InstaHome/>
+    <PartnerHome/>
+    <FooterHome/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+import Header from "@/components/Header.vue";
+import CarouselHome from "@/components/CarouselHome.vue";
+import ItemHome from "@/components/ItemHome.vue";
+import InstaHome from "@/components/InstaHome.vue";
+import PartnerHome from "@/components/PartnerHome.vue";
+import FooterHome from "@/components/FooterHome.vue";
+
+export default {
+  name: "Home",
+  components: {
+    Header,
+    CarouselHome,
+    ItemHome,
+    InstaHome,
+    PartnerHome,
+    FooterHome,
+  },
+};
+</script>
